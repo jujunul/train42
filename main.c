63ee45fce6cb36ad552fd038c362f65467659366
jujunull <jujunull>
2019-07-11 16:36:27 +0200

    ft atoi

--- main.c
+++ main.c
@@ -2,85 +2,44 @@
 
 void	ft_putchar(char c);
 
-
-void	ft_first_line(int x, char *tab)
+int		ft_isspace(char c)
 {
-	int i;
-	
-	i = 1;
-	ft_putchar(tab[0]);
-	while (i < x - 1)
-	{
-		ft_putchar(tab[4]);
-		i++;
-	}
-	if (x >= 2)
-		ft_putchar(tab[1]);
-	ft_putchar('\n');
-}
-
-void	ft_middle(int x, int y, char *tab)
-{
-	int i;
-	int j;
-	
-	i = 1;
-	while (i < y - 1)
-	{
-		j = 1;
-		ft_putchar(tab[5]);
-		while (j < x - 1 )
-		{
-			ft_putchar(' ');
-			j++;
-		}
-		if (x > 1)
-			ft_putchar(tab[5]);
-		ft_putchar('\n');
-		i++;
-	}
-}
-
-void	ft_last_line(int x, char *tab)
-{
-	int i;
-	
-	i = 1;
-	ft_putchar(tab[2]);
-	while (i < x - 1)
-	{
-		ft_putchar(tab[4]);
-		i++;
-	}
-	if (x >= 2)
-		ft_putchar(tab[3]);
-	ft_putchar('\n');
-}
-
-void	rush(int x, int y)
-{
-	char	tab[6];
-	
-	tab[0] = '/';
-	tab[1] = '\\';
-	tab[2] = '\\';
-	tab[3] = '/';
-	tab[4] = '*';
-	tab[5] = '*';
-	
-	if (x > 0 && y > 0)
-	{
-		ft_first_line (x, tab);
-		if (y > 2)
-			ft_middle(x, y, tab);
-		if (y >= 2)
-			ft_last_line (x, tab);
-	}
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v')
+		return (1);
 	else
-		printf("don't troll me\n");
+		return (0);
+}
+
+
+int		ft_atoi(char *str)
+{
+	int		i;
+	int		neg;
+	long	nb;
+
+	i = 0;
+	neg = 1;
+	nb = 0;
+	while (ft_isspace(str[i]) != 0)
+	{
+		i++;
+	}
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			neg *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (nb * 10) + (str[i] % 48);
+		i++;
+	}
+	nb *= neg;
+	return ((int)nb);
 }
 
 int main(void)
 {
-	rush(0, 10);
+	printf("%d\n", ft_atoi("+++œ153a00"));
 }
